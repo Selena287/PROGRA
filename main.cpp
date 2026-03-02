a92901ca9a8bbdc37c6d5bd7237baa475649a0c0
Selena287 <mvelasquezo5@miumg.edu.gt>
2026-03-01 19:44:52 -0600

    Add files via upload
    HOJA DE TRABAJO2

---
+++ main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;        //Curso: Programación III
+                            //Carnet: 9941 24 14056
+                            // Maura Selena Velásquez Ortega
+                            // Semana IV – Laboratorio Producto
+struct Producto {
+    int codigo;
+    string nombre;
+    double precio;
+};
+
+struct Nodo {
+    Producto data;
+    Nodo* siguiente;
+};
+
+int main() {
+    Nodo* primero = new Nodo;
+
+    primero->data.codigo = 101;
+    primero->data.nombre = "Laptop";
+    primero->data.precio = 5500.50;
+
+    primero->siguiente = nullptr;
+
+
+    Nodo* segundo = new Nodo;
+
+    segundo->data.codigo = 102;
+    segundo->data.nombre = "Mouse";
+    segundo->data.precio = 150.75;
+
+    segundo->siguiente = nullptr;
+
+    primero->siguiente = segundo;
+
+
+    Nodo* tercero = new Nodo;
+
+    tercero->data.codigo = 103;
+    tercero->data.nombre = "Teclado";
+    tercero->data.precio = 300.00;
+
+    tercero->siguiente = nullptr;
+
+    segundo->siguiente = tercero;
+
+
+    Nodo* actual = primero;
+
+    while (actual != nullptr) {
+        cout << "Codigo: " << actual->data.codigo << endl;
+        cout << "Nombre: " << actual->data.nombre << endl;
+        cout << "Precio: Q" << actual->data.precio << endl;
+        cout << "--------------------------" << endl;
+
+        actual = actual->siguiente;
+    }
+
+
+    actual = primero;
+
+    while (actual != nullptr) {
+        Nodo* temp = actual;
+        actual = actual->siguiente;
+        delete temp;
+    }
+
+    primero = nullptr;
+
+    return 0;
+}
